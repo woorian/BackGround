a9e6b399d1a14a476475370c60583a4a9530358c
woorian <woorian@siso.io>
2019-04-03 18:21:57 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.035453a855c1a333ba06f186cf75bba1.js"
+  "/precache-manifest.a23b2e0667e6d7702d1bb3734fea9854.js"
 );
 
 workbox.clientsClaim();
